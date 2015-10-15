40d703edcf18925a5981d24ad6ca33e118d2f3b1
Nicholas <nicholas_kh@foxmail.com>
2015-10-15 16:22:58 +0800

    Nlib v0.1.20151015

--- shape.h
+++ shape.h
@@ -1,8 +1,31 @@
 #ifndef _SHAPE_H
 #define _SHAPE_H
 
-#include "Nilb.h"
+#include <SDL2/SDL.h>
+#include <math.h>
 
+using rectangle = SDL_Rect;
+using point = SDL_Point;
 
+struct vec {
+	int dx, dy;
+};
+
+struct line {
+	point vert; //vertex
+	vec dir; //direction
+};
+
+struct circle {
+	point c;
+	int r;
+};
+
+int distance(const point& m, const point& n);
+
+point& operator+=(point& p, const vec& v);
+point operator+(const point& p, const vec& v);
+
+int operator*(const vec& a, const vec& b);
 
 #endif
