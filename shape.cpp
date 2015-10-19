7b8cb6131a3dd0f831864fce9a83c6b3981f4b15
Nicholas <nicholas_kh@foxmail.com>
2015-10-20 00:15:17 +0800

    Nlib v0.1.20151020
    1

--- shape.cpp
+++ shape.cpp
@@ -6,15 +6,32 @@ std::vector<rectangle>& circle::shape(int pixel)
 	rectangle rect;
 	if (int(2*r / pixel) == 0)
 	{
-		rect = {c.x - r, c.y - r, }
-		shape.push_back({})
-		
+		rect = {c.x - r/sqrt(2), c.y - r/sqrt(2), 2*r/sqrt(2), 2*r/sqrt(2)};
+		shape.push_back(rect);
 	}
-	int x = 2*r, y;
-	while (x / pixel != 0 || x % pixel != 0)
-	{
-		
-		x -= pixel
+	else {
+		int w = 2*r - pixel/2, h, left, right;
+		rect = {c.x - r + pixel/2, 0, pixel, 0};
+		relt = sqrt(pow(r, 2) - pow(w - r, 2));
+		while (int(w / pixel) != 0)
+		{
+			w -= pixel;
+			right = sqrt(pow(r, 2) - pow(w - r, 2));
+			rect.h = left < right ? 2*letf : 2*right;
+			rect.y = c.y - rect.h/2;
+			shape.push_back(rect);
+			rect.x += pixel;
+			left = right;
+		}
+		if (w % pixel != 0)
+		{
+			rect.w = w % pixel;
+			right = sqrt(pow(r, 2) - pow(w - r, 2));
+			rect.h = left < right ? 2*letf : 2*right;
+			rect.y = c.y - rect.h/2;
+			shape.push_back(rect);
+		}
 	}
+	return shape;
 }
 
