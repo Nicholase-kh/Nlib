4627e885d5b03231fe7b37c5bc4a41c2893d02c7
Nicholas <nicholas_kh@foxmail.com>
2015-10-22 22:56:41 +0800

    Nlib v0.1.20151022
    2

--- box.cpp
+++ box.cpp
@@ -1,1 +1,99 @@
 #include "box.h"
+
+box::box(const std::vector<rectangle>& box): _box(box)
+{
+	if (!box.empty()) {
+		rectangle rect = box[0];
+		_c = {rect.x, rect.y};
+		for (int i = 0; i != box.size(); ++i)
+		{
+			if (rect.x >= box[i].x)
+				rect.x = box[i].x;
+			if (rect.y >= box[i].y)
+				rect.y = box[i].y;
+			if (rect.x + rect.w <= box[i].x + box[i].w)
+				rect.w = box[i].x + box[i].w - rect.x;
+			if (rect.y + rect.h <= box[i].y + box[i].h)
+				rect.h = box[i].y + box[i].h - rect.y;
+		}
+		_box.push_front(rect);
+	}else {
+		_c = {0, 0};
+	}
+}
+
+box::box(const vec& v, const std::vector<rectangle>& box): _v(v), _box(box)
+{
+	if (!box.empty()) {
+		rectangle rect = box[0];
+		_c = {rect.x, rect.y};
+		for (int i = 0; i != box.size(); ++i)
+		{
+			if (rect.x >= box[i].x)
+				rect.x = box[i].x;
+			if (rect.y >= box[i].y)
+				rect.y = box[i].y;
+			if (rect.x + rect.w <= box[i].x + box[i].w)
+				rect.w = box[i].x + box[i].w - rect.x;
+			if (rect.y + rect.h <= box[i].y + box[i].h)
+				rect.h = box[i].y + box[i].h - rect.y;
+		}
+		_box.push_front(rect);
+	}else {
+		_c = {0, 0};
+	}
+}
+
+void box::move()
+{
+	for (auto& rect : _box)
+	{
+		_box.x += _v.dx;
+		_box.y += _v.dy;
+	}
+	mov_box(_v);
+}
+
+void box::moveT(const vec& t)
+{
+	for (auto& rect : _box)
+	{
+		_box.x += t.dx;
+		_box.y += t.dy;
+	}
+	mov_box(t);
+	_v = { 0, 0 };
+}
+
+void box::acc(const vec& a)
+{
+	_v += a;
+}
+
+bool box::InBox(const point& p)
+{
+	if (!_box.empty())
+	{
+		for (auto rect : _box)
+		{
+			if (p.x - rect.x <= rect.w)
+				if(p.y - rect.y <= rect.h)
+					return true;
+		}
+		return false;
+	}else {
+		return p == _c;
+	}
+}
+
+std::vector<rectangle>&& box::Box() const
+{
+	auto box = _box;
+	if (!box.empty())
+		box.pop_front();
+	return std::move(box);
+}
+
+
+
+
--- box.h
+++ box.h
@@ -2,6 +2,7 @@
 #define BOX_H
 
 #include <vector>
+#include <utility>
 #include <SDL2/SDL.h>
 
 #include "shape.h"
@@ -15,16 +16,18 @@ public:
 	box(const vec& v, const std::vector<rectangle>& box);
 	
 	void move();
-	void moveT(const point& p); //move by touch
+	void moveT(const vec& t); //move by touch
 	void acc(const vec& a); //accelerate
-	
-	inline std::vector<rectangle> Box() const { return _box; }
+	bool InBox(const point& p);
+	std::vector<rectangle>&& Box() const;
 private:
+	inline void mov_box(const vec& v) { _c += v; }
 	std::vector<rectangle> _box;
 	point _c;
 	vec _v = 0;
 };
 
+box mirror(const box& dot, const line& l);
 
-
+bool overlap(const std::vector<rectangle>& boxA, const std::vector<rectangle>& boxB);
 #endif
